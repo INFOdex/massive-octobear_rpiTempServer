d18cc1238a7bd457a183a49654d627061a548346
Peter George Lenardis <peter.lenardis@maine.edu>
2015-04-28 23:01:48 +0000

    added the php file that creates the webpage from other files

---
+++ display.php
@@ -0,0 +1,10 @@
+<HTML>
+<BODY BACKGORUND="pug.jpg">
+
+<?php
+echo "<center><TABLE BGCOLOR = \"FFFFFF\" border=2><TR>";
+echo "<TD><img src=\"grpah.jpg\"></TD>";
+echo "</TR></TABLE><center>";
+?>
+
+</HTML>
